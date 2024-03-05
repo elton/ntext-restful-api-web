f2507e382316c49a7e57114ea02d9210dc1d3843
Elton Zheng <elton.zheng.sh@gmail.com>
2024-03-05 15:56:26 +0800

    init the demo web app

---
+++ uno.config.ts
@@ -0,0 +1,33 @@
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  /**
+   * 自定义快捷语句
+   * @see https://github.com/unocss/unocss#shortcuts
+   */
+  shortcuts: [
+    { 'i-logo': 'i-logos-astro w-6em h-6em transform transition-800' },
+  ],
+  transformers: [
+    // 启用 @apply 功能
+    transformerDirectives(),
+    // 启用 () 分组功能
+    // 支持css class组合，eg: `<div class="hover:(bg-gray-400 font-medium) font-(light mono)">测试 unocss</div>`
+    transformerVariantGroup(),
+  ],
+  presets: [
+    presetUno(),
+    presetIcons({
+      extraProperties: {
+        display: 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
+  ],
+})
