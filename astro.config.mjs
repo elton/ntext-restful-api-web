439dc2ebe2116cda52e3445e54ab452173f0c100
Elton Zheng <elton.zheng.sh@gmail.com>
2024-03-06 23:10:03 +0800

    add astro-compress module to compress static files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,27 @@
 import node from '@astrojs/node'
 import solid from '@astrojs/solid-js'
+import compress from 'astro-compress'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({ injectReset: true }), solid()],
   output: 'hybrid',
-  server: {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
+  // server: {
+  //   headers: {
+  //     'Access-Control-Allow-Origin': '*',
+  //   },
+  // },
   adapter: node({
     mode: 'standalone',
   }),
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    solid(),
+    // AstroCompress will not compress your requests, only your statically generated build and pre-rendered routes.
+    // Use AstroCompress last in your integration list for the best optimization.
+    compress(),
+  ],
 })
